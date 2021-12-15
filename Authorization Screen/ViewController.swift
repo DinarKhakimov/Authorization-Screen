0ab113fa24e17d5d2e4adbd7179d63d8d4140081
Johnny Boshechka <in_order@icloud.com>
2021-12-15 19:47:02 +0300

    Initial Commit

---
+++ Authorization Screen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Authorization Screen
+//
+//  Created by Johnny Boshechka on 12/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
