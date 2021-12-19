f4e294e31ed3b51dd521363ac9ca73dd97d6442b
Johnny Boshechka <in_order@icloud.com>
2021-12-19 18:47:30 +0500

    Correcting errors after parsing homework.

--- Authorization Screen/LoginViewController.swift
+++ Authorization Screen/LoginViewController.swift
@@ -8,41 +8,70 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    // MARK: - IB Outlets
     @IBOutlet weak var userNameLabel: UITextField!
     @IBOutlet weak var passwordLabel: UITextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+    // MARK: - Private properties
+    private var user = "Johnny"
+    private var password = "777"
     
+    // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let welcomeViewController = segue.destination as? WelcomeViewController else { return }
-        welcomeViewController.userName = userNameLabel.text
-    }
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesBegan(touches, with: event)
-        self.view.endEditing(true)
+        welcomeViewController.userName = user
     }
     
-    @IBAction func loginButton(_ sender: UIButton) {
+    
+    
+    // MARK: - IB Actions
+    @IBAction func loginPressed() {
+        if userNameLabel.text != user || passwordLabel.text != password {
+            showAlert(title: "Invalid login or password",
+                      message: "Please, enter correct login and password",
+                      textField: passwordLabel)
+        }
     }
-    @IBAction func forgotUserNameButton(_ sender: UIButton) {
-        showAlert(title: "User Name", message: "Boby")
+    @IBAction func forgotUserData(_ sender: UIButton) {
+        sender.tag == 0
+        ? showAlert(title: "Help", message: "Your password is \(password)")
+        : showAlert(title: "Help", message: "Your user name is \(user)")
     }
-    @IBAction func forgotPasswordButton(_ sender: Any) {
-        showAlert(title: "Password", message: "777")
-    }
+    
     @IBAction func unwind(for: UIStoryboardSegue) {
         userNameLabel.text = ""
         passwordLabel.text = ""
     }
 }
 
+// MARK: - Extensions & Show Alert Controllera
 extension LoginViewController {
-    private func showAlert(title: String, message: String) {
+    private func showAlert(title: String, message: String, textField: UITextField? = nil) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "Ok", style: .default)
+        let okAction = UIAlertAction(title: "Ok", style: .default) { _ in
+            textField?.text = ""
+        }
         alert.addAction(okAction)
         present(alert, animated: true)
+    }
+    
+}
+
+// MARK: - Setup Text Field
+extension LoginViewController: UITextFieldDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == userNameLabel {
+            passwordLabel.becomeFirstResponder()
+        } else {
+            loginPressed()
+            performSegue(withIdentifier: "showWelcomViewController", sender: nil)
+        }
+        return true
     }
 }
--- Authorization Screen/WelcomeViewController.swift
+++ Authorization Screen/WelcomeViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
     
+    // MARK: - property
     @IBOutlet weak var welcomeLabel: UILabel!
     
-    var userName: String!
-    let welcome = "Welcome, "
+    var userName = ""
+    
+    private var leftSideColor = UIColor(red: 117/255,
+                                   green: 130/255,
+                                   blue: 120/255,
+                                   alpha: 1)
+    private var rightSideColor = UIColor(red: 180/255,
+                                      green: 49/255,
+                                      blue: 34/255,
+                                      alpha: 1)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        welcomeLabel.text = welcome + userName
+        view.addHorizontGradientLayer(leftSideColor: leftSideColor, rightSideColor: rightSideColor)
+        welcomeLabel.text = "Welcome \(userName)!"
     }
-    @IBAction func logOutButton(_ sender: UIButton) {
+}
+
+// MARK: - Set background color
+extension UIView {
+    func addHorizontGradientLayer(leftSideColor: UIColor, rightSideColor: UIColor) {
+        let gradient = CAGradientLayer()
+        gradient.frame = bounds
+        gradient.colors = [leftSideColor.cgColor, rightSideColor.cgColor]
+        gradient.locations = [0.0, 1.0]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 1, y: 0)
+        layer.insertSublayer(gradient, at: 0)
     }
 }
